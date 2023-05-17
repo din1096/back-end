4f279d731dd488ade15e78ba649d0fdaa15119da
dinho <99069865@mydavinci.nl>
2023-05-17 09:23:01 +0200

    'update'

---
+++ rekenen.php
@@ -0,0 +1,10 @@
+<?php
+$number1 = (rand(10,100));
+$number2 = (rand(10,100));
+
+$optellen = $number1 + $number2;
+$aftrekken = $number1 - $number2;
+$keer = $number1 * $number2;
+$delen = $number1 / $number2;
+echo nl2br ("$number1\n$number2\n$optellen\n$aftrekken\n$keer\n$delen");
+?>
