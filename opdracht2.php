4583272bccf7054574311eaba61f40652d955497
dinho <99069865@mydavinci.nl>
2023-05-25 11:02:35 +0200

    'update'

---
+++ opdracht2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<body>
+    <?php
+function letters_tellen($woorden) {
+    foreach ($woorden as $woord) {
+        if 
+    }
+
+}
+
+
+
+
+$woordenlijst = ['kat', 'hond', 'olifant']
+    ?>
+
+</body>
+</htlm>
