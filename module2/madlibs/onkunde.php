5ea9d6e68da733de8a58d52c0a641acdc43fd2b3
dinho <99069865@mydavinci.nl>
2023-06-28 09:12:24 +0200

    'update'

--- module2/madlibs/onkunde.php
+++ module2/madlibs/onkunde.php
@@ -12,41 +12,41 @@
             <li><a href="onkunde.php">Onkunde</a></li>
             </ul>
         <?php
-        $p1fout = $p2fout = $p3fout = $p4fout = $p5fout = $p6fout = $p7fout = "";
+        $fout1 = $fout2 = $fout3 = $fout4 = $fout5 = $fout6 = $fout7 = "";
         $p1 = $p2 = $p3 = $p4 = $p5 = $p6 = $p7 = "";
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (empty($_POST["p1"])) {
-                $p1fout = "antwoord is verplicht!";
+                $fout1 = "answer is required";
             } else {
                 $p1 = test_input($_POST["p1"]);
             }
             if (empty($_POST["p2"])) {
-                $p2fout = "antwoord is verplicht!";
+                $fout2 = "answer is required";
             } else {
                 $p2 = test_input($_POST["p2"]);
             }
             if (empty($_POST["p3"])) {
-                $p3fout = "antwoord is verplicht!";
+                $fout3 = "answer is required";
             } else {
                 $p3 = test_input($_POST["p3"]);
             }
             if (empty($_POST["p4"])) {
-                $p4fout = "antwoord is verplicht!";
+                $fout4 = "answer is required";
             } else {
                 $p4 = test_input($_POST["p4"]);
             }
             if (empty($_POST["p5"])) {
-                $p5fout = "antwoord is verplicht!";
+                $fout5 = "answer is required";
             } else {
                 $p5 = test_input($_POST["p5"]);
             }
             if (empty($_POST["p6"])) {
-                $p6fout = "antwoord is verplicht!";
+                $fout6 = "answer is required";
             } else {
                 $p6 = test_input($_POST["p6"]);
             }
             if (empty($_POST["p7"])) {
-                $p7fout = "antwoord is verplicht!";
+                $fout7 = "answer is required";
             } else {
                 $p7 = test_input($_POST["p7"]);
             }
@@ -60,25 +60,25 @@
             ?>
                 <form  class="tekst" method="post" action="resultaatonkunde.php">  
                 Wat zou je graag willen kunnen? <input type="text" name="p1">
-                <span>* <?php echo $p1fout;?></span>
+                <span>* <?php echo $fout1;?></span>
                 <br><br>
                 Met welke persoon kun je goed opschieten? <input type="text" name="p2">
-                <span>* <?php echo $p2fout;?></span>
+                <span>* <?php echo $fout2;?></span>
                 <br><br>
                 Wat is je favoriete getal? <input type="text" name="p3">
-                <span>* <?php echo $p3fout;?></span>
+                <span>* <?php echo $fout3;?></span>
                 <br><br>
                 Wat heb je altijd bij je als je op vakantie gaat? <input type="text" name="p4">
-                <span>* <?php echo $p4fout;?></span>
+                <span>* <?php echo $fout4;?></span>
                 <br><br>
                 Wat is je beste persoonlijke eigenschap? <input type="text" name="p5">
-                <span>* <?php echo $p5fout;?></span>
+                <span>* <?php echo $fout5;?></span>
                 <br><br>
                 Wat is je slechtste persoonlijke eigenschap? <input type="text" name="p6">
-                <span>* <?php echo $p6fout;?></span>
+                <span>* <?php echo $fout6;?></span>
                 <br><br>
                 Wat is het ergste wat je kan overkomen? <input type="text" name="p7">
-                <span>* <?php echo $p7fout;?></span>
+                <span>* <?php echo $fout7;?></span>
                 <br><br>    
                 <input type="submit" name="submit" value="Versturen">  
                 </form>
