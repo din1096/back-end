82710b960f5154cb8fd8d53a5c22565f9af9bd89
dinho <99069865@mydavinci.nl>
2024-05-29 10:15:57 +0200

    finish the database

---
+++ jukebox/Dinho/app/Http/Controllers/Genre.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Genre extends Controller
+{
+    //
+}
--- jukebox/Dinho/routes/web.php
+++ jukebox/Dinho/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\welcome; 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/hello", [welcome::class, "hello"]);
+Route::get("/song",); 
